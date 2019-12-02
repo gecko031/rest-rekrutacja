5595e2b8b9f01bd20d4e08a5d92ac1521171abab
pawel <pawel@pop-os.localdomain>
2019-12-02 20:08:21 +0100

    incrementation of version document field

--- models/note.models.js
+++ models/note.models.js
@@ -2,12 +2,18 @@ let mongoose = require('mongoose');
 
 const noteSchema = mongoose.Schema(
     {
+        _id: Number,
         title: String,
-        content: String
+        content: String,
+        __v: Number
     },
     {   
         timestamps: true
     }
 );
+noteSchema.pre('findOneAndUpdate', function(next) {
+    this.update({}, {$inc: {__v: 0.5}}, next );
+})
+
 
 module.exports = mongoose.model('NoteModel', noteSchema)
