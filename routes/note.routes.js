1bb8765e062f9bb21e24debd3f0eac162b238390
pawel <pawel@pop-os.localdomain>
2019-12-28 12:11:20 +0100

    added webservice routing to 'historyNotes' collection

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,10 +1,16 @@
 module.exports = (app) => {
     const NoteModel = require('../controllers/note.controllers.js');
+    const HistoryNoteModel = require('../controllers/history.controllers.js')
     
+    // Current collection
     app.get('/ps/notes', NoteModel.getAllNotes);
     app.get('/ps/notes/:id', NoteModel.getNote);
     app.post('/ps/notes', NoteModel.createNote);
     app.put('/ps/notes/:id', NoteModel.updateNote);
     app.delete('/ps/notes/:id', NoteModel.deleteNote);
+
+    // History collection
+    app.get('/ps/history', HistoryNoteModel.getAllNotes);
+    app.get('/ps/history/:id', HistoryNoteModel.getNote);
 
 }
